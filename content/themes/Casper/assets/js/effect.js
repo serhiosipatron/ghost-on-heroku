96145c093538f07443828e56c16b958b7c3d8371
Sergey Sipatrin <ssipatrin@webinerds.com>
2019-03-29 23:28:15 +0200

    'post-height'

--- content/themes/Casper/assets/js/effect.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function () {
-    var images = document.querySelectorAll('.thumbnail');
-    new simpleParallax(images, {
-        delay: 0,
-        orientation: 'down',
-        overfow: true
-    });;
-});
